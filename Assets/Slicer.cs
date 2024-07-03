9b8b8f63daf7dfa125306b18825c41d1a0271db5
meepodeep <nbltmepodep@gmail.com>
2024-07-02 22:37:01 -0400

    made the slicer cut things

--- Assets/Slicer.cs
+++ Assets/Slicer.cs
@@ -1,22 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SocialPlatforms;
+
 
 public class Slicer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    // all of the foods sliced//
+    public GameObject OnionCut;
+    //public GameObject BurgerCut;
+    //public GameObject PotatoCut;
+    public GameObject LettuceCut;
+    private Vector3 SpawnVector; 
+    public GameObject[] foodsCut; 
+    void Start(){
+        foodsCut = new GameObject[10];
+        foodsCut[0] = LettuceCut;
+        foodsCut[1] = OnionCut;
     }
+    public void OnTriggerEnter(Collider other)
+    {
+        SpawnVector = other.gameObject.transform.position;
+        
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log(other.tag); 
+        switch(other.tag){
+            case "Onion":
+            Destroy(other.gameObject);
+            Instantiate(foodsCut[1], SpawnVector, Quaternion.identity); 
+            Instantiate(foodsCut[1], SpawnVector, Quaternion.identity);
+            break;
+            case "Lettuce":
+            Destroy(other.gameObject);
+            Instantiate(foodsCut[0], SpawnVector, Quaternion.identity); 
+            Instantiate(foodsCut[0], SpawnVector, Quaternion.identity); 
+            break;
+
+        }
     }
 }
