61e4bcf1327b10493b8413c0591a1193a19ce9ea
meepodeep <nbltmepodep@gmail.com>
2024-07-13 02:07:56 -0400

    made a distance grab script

--- Assets/PullObject.cs
+++ Assets/PullObject.cs
@@ -1,21 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering;
-
+using UnityEngine.InputSystem;
 public class PullObject : MonoBehaviour
 {
     Ray ray;
     RaycastHit hit; 
+    public Transform hand; 
+    public LayerMask layersToHit;
+    public float maxDistance = 100;
+    public InputActionProperty grabInputSource;
     // Start is called before the first frame update
     void Start()
     {
     
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    void FixedUpdate()
     {
-        
+        bool isGrabButtonPressed = grabInputSource.action.ReadValue<float>() > 0.1f;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(hand.position, hand.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layersToHit))
+        {
+            if (hit.distance <= 2 && isGrabButtonPressed){
+                hit.collider.transform.position = hand.transform.position;
+            }
+            Debug.Log(hit.distance);
+            Debug.DrawRay(hand.position, hand.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log(hit.collider.gameObject.name + "Did Hit");
+        }
     }
 }
